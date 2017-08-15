9bde33ad2a9d021b05025e4a798253b61f6f6512
DaveJag <davejag@comcast.net>
2017-08-15 16:57:16 -0400

    Initial files created.

---
+++ keys.js
@@ -0,0 +1,11 @@
+// Dave Jagodowski - HW08
+// keys.js file
+
+console.log('This is loaded.');
+
+exports.twitterKeys = {
+	consumer_key: 'nLiPQy3HVtrpyX4f0uB9vmdxK',
+	consumer_secret: 'Uurhy2TJSBJmzTGRHso0lSd6c0JBS5plOtP7iMBiZvOExVj7CO',
+    access_token_key: '897120956329713666-D5eVp9iIodVHiOdvWe1Fh86CFQktHCa',
+    access_token_secret: 'dWm8h0uAl3Wws0udIwetY3MogfcPoHyKT8XDqvuDSYsRZ',	
+}
---
+++ liri.js
@@ -0,0 +1,8 @@
+//Dave Jagodowski HW08
+// liri.js
+var twitKeys = require("./keys.js");
+var consumerKey = twitterKeys.consumer_key;
+var consumerSecret = twitterKeys.consumer_secret;
+var accessTokenKey = twitterKeys.access_token_key;
+var accessTokenSecret = twitterKeys.access_token_secret;
+  
